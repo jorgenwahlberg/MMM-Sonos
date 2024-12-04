376f1991d787b9c05513e1c0a627ba5677a5614c
Jørgen Veum-Wahlberg <jorgen.wahlberg@amedia.no>
2024-12-04 21:30:06 +0100

    Changed from request to built-in function

--- node_helper.js
+++ node_helper.js
@@ -6,24 +6,54 @@
  * MIT Licensed.
  */
 var NodeHelper = require('node_helper');
-var request = require('request');
+var http = require('http');
+var https = require('https');
+var url = require('url');
 
 module.exports = NodeHelper.create({
   start: function () {
     console.log('Sonos helper started ...');
   },
-  //Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, url) {
+
+  // Subclass socketNotificationReceived.
+  socketNotificationReceived: function(notification, targetUrl) {
     if (notification === 'SONOS_UPDATE') {
-      //console.log(notification);
-      var self = this;
-      request(url, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          self.sendSocketNotification('SONOS_DATA', JSON.parse(body));
-        } else {
-      		console.error('Failure: ' + error);
-        }
+      const self = this;
+
+      // Parse the URL to determine the protocol.
+      const parsedUrl = new url.URL(targetUrl);
+      const protocol = parsedUrl.protocol === 'https:' ? https : http;
+
+      // Make the HTTP or HTTPS request.
+      const req = protocol.get(targetUrl, (res) => {
+        let data = '';
+
+        // Accumulate data chunks.
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        // Handle response end.
+        res.on('end', () => {
+          if (res.statusCode === 200) {
+            try {
+              self.sendSocketNotification('SONOS_DATA', JSON.parse(data));
+            } catch (err) {
+              console.error('Error parsing JSON:', err);
+            }
+          } else {
+            console.error(`Request failed. Status code: ${res.statusCode}`);
+          }
+        });
       });
+
+      // Handle request errors.
+      req.on('error', (err) => {
+        console.error('Request error:', err);
+      });
+
+      // End the request.
+      req.end();
     }
   }
 });
