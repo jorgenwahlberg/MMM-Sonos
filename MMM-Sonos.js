7de39ed4521c8f9b390d07be7104ff49e4210dc7
Jørgen Veum-Wahlberg <jorgen@wahlberg.no>
2025-10-28 22:35:30 +0100

    Using template for rendering

--- MMM-Sonos.js
+++ MMM-Sonos.js
@@ -24,11 +24,23 @@
 	},
 	start: function() {
 		Log.info('Starting module: ' + this.name);
+		this.loaded = false;
+		this.rooms = [];
 		this.update();
 		// refresh every x minutes
 		setInterval(
 			this.update.bind(this),
 			this.config.updateInterval * 60 * 1000);
+	},
+	getTemplate: function() {
+		return 'MMM-Sonos.njk';
+	},
+	getTemplateData: function() {
+		return {
+			loading: !this.loaded,
+			flip: this.data.position.endsWith("left"),
+			rooms: this.rooms
+		};
 	},
 	update: function(){
 		this.sendSocketNotification('SONOS_UPDATE',this.config.apiBase + ":" + this.config.apiPort + "/" + this.config.apiEndpoint);
@@ -46,110 +58,78 @@
 		return true;
 	},
 	render: function(data){
-		var text = '';
-		$.each(data, function (i, item) {
-			var room = '';
+		var rooms = [];
+		var self = this;
+
+		data.forEach(function(item) {
+			var roomName = '';
 			var isGroup = item.members.length > 1;
 			if(isGroup){
-				$.each(item.members, function (j, member) {
-					var shouldShow = this.shouldShowRoom(member.roomName);
-					room += shouldShow?(member.roomName + ', '):'';
-				}.bind(this));
-				room = room.replace(/, $/,"");
+				item.members.forEach(function(member) {
+					var shouldShow = self.shouldShowRoom(member.roomName);
+					roomName += shouldShow?(member.roomName + ', '):'';
+				});
+				roomName = roomName.replace(/, $/,"");
 			}else{
-				room = item.coordinator.roomName;
-				var shouldShow = this.shouldShowRoom(room);
-				room = shouldShow?room:'';
+				roomName = item.coordinator.roomName;
+				var shouldShow = self.shouldShowRoom(roomName);
+				roomName = shouldShow?roomName:'';
 			}
-			if(room !== ''){
+			if(roomName !== ''){
 				var state = item.coordinator.state.playbackState;
 				var currentTrack = item.coordinator.state.currentTrack;
 				var artist = currentTrack.artist;
 				var track = currentTrack.title;
 				var cover = currentTrack.absoluteAlbumArtUri;
-				var streamInfo = currentTrack.streamInfo;
-				var type = currentTrack.type;
 
-				if(track == currentTrack.uri)
+				// Handle special cases
+				if (!artist && !track && !cover) {
+					artist = "TV";
+				}
+				if (track && track.indexOf("x-sonosapi-stream:") == 0) {
+					track = undefined;
+				}
+				if (track === ".") {
+					track = undefined;
+				}
+				if(track == currentTrack.uri) {
 					track = '';
-				text += this.renderRoom(state, artist, track, cover, room);
+				}
+
+				artist = artist?artist:"";
+				cover = cover?cover:"";
+
+				// Only add room if PLAYING
+				if(state === 'PLAYING') {
+					rooms.push({
+						state: state,
+						artist: artist,
+						track: track,
+						cover: cover,
+						roomName: roomName,
+						showAlbumArt: self.config.showAlbumArt,
+						showRoomName: self.config.showRoomName,
+						showStoppedRoom: self.config.showStoppedRoom
+					});
+				}
 			}
-		}.bind(this));
+		});
+
 		this.loaded = true;
-		// only update dom if content changed
-		if(this.dom !== text){
+		this.rooms = rooms;
+
+		// Update DOM
+		this.updateDom(this.config.animationSpeed);
+
+		// Show/hide module based on playing rooms
+		if(rooms.length > 0){
 			this.show();
-			this.dom = text;
-			this.updateDom(this.config.animationSpeed);
-		}
-		// Hide module if not playing.
-		if(text == ''){
+		} else {
 			this.hide(this.config.animationSpeed);
 		}
 	},
-	renderRoom: function(state, artist, track, cover, roomName) {
-		if (!artist && !track && !cover) {
-			artist = "TV";
-		}
-		if (track && track.indexOf("x-sonosapi-stream:") == 0) {
-			track = undefined;
-		}
-		if (track === (".")) {
-			track = undefined;
-		}
-		artist = artist?artist:"";
-		//track = track?track:"";
-		cover = cover?cover:"";
-		var room = '';
-
-
-		// show song if PLAYING
-		if(state === 'PLAYING') {
-			room += this.html.song.format(
-				// show album art if 'showAlbumArt' is set
-				(this.config.showAlbumArt
-					?this.html.art.format(cover)
-					:''
-				)+				
-				this.html.artistBlock.format(
-					(track ? this.html.artistAndTrack.format(artist, track) : this.html.artist.format(artist, track))
-					
-				+ (this.config.showRoomName && (state === 'PLAYING' || this.config.showStoppedRoom) ? this.html.room.format(roomName) : ""
-				//+"<span>"+streamInfo+"</span>"
-				))
-			);
-		}
-		return  this.html.roomWrapper.format(room);
-	},
-	html: {
-		loading: '<div class="dimmed light small">Loading music ...</div>',
-		roomWrapper: '<li>{0}</li>',
-		room: '<div class="room xsmall">{0}</div>',
-		song: '<div class="song">{0}</div>',
-		artistBlock: '<div class="name normal medium">{0}</div>',
-		artist: '<div class="artist">{0}</div>',
-		artistAndTrack: '<div class="artist">{0}</div><div class="track">{1}</div>',
-		art: '<div class="art"><img src="{0}"/></div>'
-	},
-	getScripts: function() {
-		return [
-			'String.format.js',
-			'//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.js'
-		];
-	},
 	getStyles: function() {
 		return ['sonos.css'];
-	},
-	getDom: function() {
-		var content = '';
-		if (!this.loaded) {
-			content = this.html.loading;
-		}else if(this.data.position.endsWith("left")){
-			content = '<ul class="flip">'+this.dom+'</ul>';
-		}else{
-			content = '<ul>'+this.dom+'</ul>';
-		}
-		return $('<div class="sonos">'+content+'</div>')[0];
 	},
 	socketNotificationReceived: function(notification, payload) {
 	if (notification === 'SONOS_DATA') {
